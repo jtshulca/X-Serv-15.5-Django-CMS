25e26063f834dd8602ea2060c7f0a55abd0d7230
jtshulca <jenifer9420@hotmail.com>
2017-04-11 12:27:54 +0200

    entrega ejercicio

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, HttpResponseNotFound
+from cms.models import Pages
 # Create your views here.
+
+
+def show(request):
+    record = Pages.objects.all()
+    respuesta = "Page Found: "
+    for page in record:
+        respuesta += "<li>/" + page.name + " --> " + page.page 
+    return HttpResponse(respuesta)
+
+
+def show_content(request, resource):
+    try:
+        record = Pages.objects.get(name=resource)
+        return HttpResponse(record.page)
+    except Pages.DoesNotExist:
+        return HttpResponseNotFound('Page Not Found: /%s.' % resource)
