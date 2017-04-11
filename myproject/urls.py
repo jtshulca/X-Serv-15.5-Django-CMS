b4a383d3cc6a258b73c4c08023e492a3ec041a45
jtshulca <jenifer9420@hotmail.com>
2017-04-11 12:29:17 +0200

    entrega ejercicio

--- myproject/urls.py
+++ myproject/urls.py
@@ -5,6 +5,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'myproject.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', 'cms.views.show', name='Paginas disponibles'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'(.+)', 'cms.views.show_content', name='Vamos a la pagina')
 )
